20f4ef2d85db5ae4000345eecb482dc1b877d11b
bartek 'basz' szurgot <git@baszerr.eu>
2022-10-18 20:16:20 +0200

    Revert "added buzzer sound by toggling on/off states"
    This reverts commit 38e51e62c8e4b1cf1ea84c2a6a41e6bfae8cd05f.

--- koala_buzzer/sw/Buzzer.hpp
+++ koala_buzzer/sw/Buzzer.hpp
@@ -4,7 +4,6 @@
 struct Buzzer
 {
   Buzzer() { DDRB |= _BV(1); }
-  void toggle() { PORTB ^=  _BV(1); }
-  void on()     { PORTB |=  _BV(1); }
-  void off()    { PORTB &= ~_BV(1); }
+  void on()  { PORTB |=  _BV(1); }
+  void off() { PORTB &= ~_BV(1); }
 };
