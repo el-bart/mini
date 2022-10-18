75980e0cf6149c6165d73a04b7321fc1dd3815fd
bartek 'basz' szurgot <git@baszerr.eu>
2022-10-18 21:47:12 +0200

    buzzer now makes an approperiate sound

--- koala_buzzer/sw/Buzzer.hpp
+++ koala_buzzer/sw/Buzzer.hpp
@@ -1,9 +1,29 @@
 #pragma once
+#include "config.hpp"
 #include <inttypes.h>
 
 struct Buzzer
 {
-  Buzzer() { DDRB |= _BV(1); }
-  void on()  { PORTB |=  _BV(1); }
-  void off() { PORTB &= ~_BV(1); }
+  Buzzer()
+  {
+    // phase correct PWM
+    TCCR0A |= _BV(WGM00);
+    //TCCR0B |= _BV(WGM02);
+    // 01 PWM
+    TCCR0A |= _BV(COM0B1);
+    // frequency ~1kHz
+    OCR0B = 20;
+    // output mode
+    DDRB |= _BV(PB1);
+  }
+
+  void on()
+  {
+    TCCR0B |= _BV(CS00); // prescaler = 1
+  }
+
+  void off()
+  {
+    TCCR0B &= ~ ( _BV(CS02) | _BV(CS01) | _BV(CS00) );  // prescaler = 0 (i.e. stop clock)
+  }
 };
