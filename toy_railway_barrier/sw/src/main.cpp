a0319c8786ebd91a0b4a103ce4f8b323befc0513
bartek 'basz' szurgot <git@baszerr.eu>
2023-05-06 10:25:34 +0200

    up and down moving barrier code

--- toy_railway_barrier/sw/src/main.cpp
+++ toy_railway_barrier/sw/src/main.cpp
@@ -2,28 +2,58 @@
 #include <Servo.h>
 #include <avr/wdt.h>
 
+namespace
+{
 Servo servo;
 auto constexpr pin_servo = 2;
+auto constexpr angle_low = 0;
+auto constexpr angle_high = +95;
+
+void wait()
+{
+  delay(1000/50); // 50Hz servo
+}
+
+void barrier_up()
+{
+  for(auto angle=angle_low; angle<=angle_high; ++angle)
+  {
+    servo.write(angle);
+    wait();
+    wdt_reset();
+  }
+}
+
+void barrier_down()
+{
+  for(auto angle=angle_high; angle>=angle_low; --angle)
+  {
+    servo.write(angle);
+    wait();
+    wdt_reset();
+  }
+}
+}
+
 
 void setup()
 {
   Serial.begin(9600);   // RX == 0, TX == 1
   servo.attach(pin_servo);
-  //barrier_opened();
+  servo.write(angle_high);
   {
     wdt_disable();
-    wdt_enable(WDTO_1S);
+    //wdt_enable(WDTO_1S);
   }
-  Serial.write("booted\n");
+  Serial.write("booted\n\r");
 }
 
 void loop()
 {
-  for(auto angle=-90; angle<90; ++angle)
-  {
-    servo.write(angle)
-    delay_ms(10);
-    // TODO
-    wdt_reset();
-  }
+  Serial.write("^");
+  barrier_down();
+  Serial.write(" ");
+  barrier_up();
+  Serial.write("v\n\r");
+  wdt_reset();
 }
