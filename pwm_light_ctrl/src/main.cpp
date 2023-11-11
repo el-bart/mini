f745c20090f122efef8c31622577b73b67ac7a66
bartek 'basz' szurgot <git@baszerr.eu>
2023-11-11 12:47:17 +0100

    PWM draft

--- pwm_light_ctrl/src/main.cpp
+++ pwm_light_ctrl/src/main.cpp
@@ -2,6 +2,7 @@
 #include <avr/wdt.h>
 
 auto constexpr pin_adc = A7;
+auto constexpr pin_pwm = A3;
 
 
 void setup()
@@ -10,7 +11,10 @@ void setup()
     wdt_disable();
     wdt_enable(WDTO_1S);
   }
+
   pinMode(pin_adc, INPUT);
+  pinMode(pin_pwm, INPUT);
+
   Serial.begin(115200);     // RX == 0, TX == 1
   Serial.write("PWM light ctrl booted\n");
 }
@@ -18,10 +22,14 @@ void setup()
 
 void loop()
 {
-  auto s = analogRead(pin_adc);
+  auto in = analogRead(pin_adc);
+  auto s = in / 4;
   Serial.print("read: ");
+  Serial.print(in, DEC);
+  Serial.print(" scaled: ");
   Serial.print(s, DEC);
   Serial.print("\n");
+  analogWrite(s);
   wdt_reset();
   delay(200);
 }
