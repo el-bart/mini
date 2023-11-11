aca29b8f4fce7fdcef0b712d09dad282684fd921
bartek 'basz' szurgot <git@baszerr.eu>
2023-11-11 13:35:38 +0100

    gamma corrected, with max resolution

--- pwm_light_ctrl/src/main.cpp
+++ pwm_light_ctrl/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <avr/wdt.h>
+#include <math.h>
 
 auto constexpr pin_adc = A7;
 auto constexpr pin_pwm = 3;
@@ -20,16 +21,37 @@ void setup()
 }
 
 
+namespace
+{
+uint8_t gamma(uint16_t in, double gamma)
+{
+  double v = in / 1023.0;
+  double out = pow(v, gamma);
+  return static_cast<uint8_t>(out * 255);
+}
+
+uint8_t clamp(uint8_t min, uint8_t value, uint8_t max)
+{
+  if(value < min)
+    return min;
+  if(max < value)
+    return max;
+  return value;
+}
+}
+
 void loop()
 {
   auto in = analogRead(pin_adc);
-  auto s = static_cast<uint8_t>(in / 4);
-  analogWrite(pin_pwm, s);
+  //auto s = static_cast<uint8_t>(in / 4);
+  auto gcs = gamma(in, 1.3);
+  auto cgcs = clamp(5, gcs, 255);
+  analogWrite(pin_pwm, cgcs);
 
   Serial.print("read: ");
   Serial.print(in, DEC);
-  Serial.print(" scaled: ");
-  Serial.print(s, DEC);
+  Serial.print(" gamma: ");
+  Serial.print(cgcs, DEC);
   Serial.print("\n");
 
   wdt_reset();
