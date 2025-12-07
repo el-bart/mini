ec361b8dfb7039d47c252ef530f29743c2166490
bartek 'basz' szurgot <git@baszerr.eu>
2025-12-07 22:58:19 +0100

    added custom cycle for on/off-s

--- toy_house_lamp/sw/main.cpp
+++ toy_house_lamp/sw/main.cpp
@@ -3,26 +3,53 @@
 #include <util/delay.h>
 #include "LED.hpp"
 
+namespace
+{
+
+void hold(LED::State& state, uint8_t const s)
+{
+  for(uint8_t j=0u; j<s; ++j)
+    for(uint8_t i=0; i<1000u/c_cycle_ms; ++i)
+      state.one_cycle();
+}
+
+
+void reach(LED::State& state)
+{
+  while(not state.stable())
+    state.one_cycle();
+}
+
+
+void cycle(LED::State& state, uint8_t on_s, uint8_t off_s)
+{
+  state.enable_fade_in();
+  reach(state);
+  hold(state, on_s);
+
+  state.enable_fade_out();
+  reach(state);
+  hold(state, off_s);
+}
+
+
+inline void execute(LED::State& state)
+{
+  cycle(state, 4, 1);
+  cycle(state, 16, 3);
+  cycle(state, 11, 2);
+  cycle(state, 21, 4);
+  cycle(state, 42, 5);
+  cycle(state, 17, 2);
+}
+
+}
+
 
 int main(void)
 {
-  //Watchdog::init();
+  Watchdog::init();
   LED::State state;
-
   while(true)
-  {
-    state.enable_fade_in();
-    while(not state.stable())
-      state.one_cycle();
-
-    for(auto i=0; i<150; ++i)
-      state.one_cycle();
-
-    state.enable_fade_out();
-    while(not state.stable())
-      state.one_cycle();
-
-    for(auto i=0; i<100; ++i)
-      state.one_cycle();
-  }
+    execute(state);
 }
