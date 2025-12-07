c6b1d48286e030babbd33fb241888f221b2ea1c6
bartek 'basz' szurgot <git@baszerr.eu>
2025-12-07 12:17:36 +0100

    moved lampshared's3D shape to a separate dir

---
+++ toy_house_lamp/lampshade/build/toy_house_lamp.stl.d
@@ -0,0 +1,16 @@
+build/toy_house_lamp.stl: \
+	/mnt/m3d/math.scad \
+	toy_house_lamp.scad \
+	/mnt/m3d/all.scad \
+	/mnt/m3d/engine/dc/mt44.scad \
+	/mnt/m3d/engine/dc/mt44_conf.scad \
+	/mnt/m3d/inner_shell.scad \
+	/mnt/m3d/map.scad \
+	/mnt/m3d/engine/stepper/nema17.scad \
+	/mnt/m3d/fn.scad \
+	/mnt/m3d/threaded_inserts/cnc_kitchen.scad \
+	/mnt/m3d/screw_head_hex.scad \
+	/mnt/m3d/engine/dc/mt83.scad \
+	/mnt/m3d/outer_shell.scad \
+	/mnt/m3d/engine/dc/mt83_conf.scad \
+	/mnt/m3d/rounded_cube.scad
