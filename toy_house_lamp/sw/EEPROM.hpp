957868cac78e1e705a0f62363bac4a5ef2a41ce4
bartek 'basz' szurgot <git@baszerr.eu>
2025-12-09 09:04:46 +0100

    added C++ compliant number to pointer conversion

--- toy_house_lamp/sw/EEPROM.hpp
+++ toy_house_lamp/sw/EEPROM.hpp
@@ -12,7 +12,7 @@ struct EEPROM
   {
     Watchdog::reset();
     wait_for_last_write_to_complete();
-    auto const byte = eeprom_read_byte((uint8_t*)uint16_t{addr});
+    auto const byte = eeprom_read_byte( to_addr(addr) );
     Watchdog::reset();
     return byte;
   }
@@ -21,7 +21,7 @@ struct EEPROM
   {
     Watchdog::reset();
     wait_for_last_write_to_complete();
-    eeprom_write_byte((uint8_t*)uint16_t{addr}, byte);
+    eeprom_write_byte( to_addr(addr), byte);
     Watchdog::reset();
   }
 
@@ -29,5 +29,12 @@ private:
   static void wait_for_last_write_to_complete()
   {
      eeprom_busy_wait();
+  }
+
+  static uint8_t* to_addr(uint8_t const addr)
+  {
+    uint8_t* ptr = nullptr;
+    ptr += addr;
+    return ptr;
   }
 };
