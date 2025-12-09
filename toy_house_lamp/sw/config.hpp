bbc89c6c0c446bf7355645f10b6455047537515d
bartek 'basz' szurgot <git@baszerr.eu>
2025-12-09 18:53:09 +0100

    more cannonical init

--- toy_house_lamp/sw/config.hpp
+++ toy_house_lamp/sw/config.hpp
@@ -8,6 +8,6 @@
 // quarc frequency in [Hz]
 #define F_CPU (1L*MHz)
 
-static constexpr uint8_t  c_cycle_ms{10};
-static constexpr uint8_t  c_gamma_steps{20};
-static constexpr uint16_t c_fade_time_ms{1500};
+static constexpr uint8_t  c_cycle_ms = 10;
+static constexpr uint8_t  c_gamma_steps = 20;
+static constexpr uint16_t c_fade_time_ms = 1500;
