72dec42cee04eb3ce91365befde2246813946ed4
bartek 'basz' szurgot <git@baszerr.eu>
2026-04-30 21:28:24 +0200

    integreated ADC-to-servo position loop

--- joysticks_to_servos/src/main.cpp
+++ joysticks_to_servos/src/main.cpp
@@ -20,11 +20,16 @@ bool pressed(int const button_pin)
   return gpio_get(button_pin) == false;
 }
 
+uint8_t volt2pos(float const v)
+{
+  auto constexpr conv_fact = 255.0f * ( 1.0f / 3.3f );
+  return static_cast<uint8_t>(v * conv_fact);
+}
+
 
 int main()
 {
   stdio_init_all();
-
   init_switches();
 
   // note: pin pairs: 2-3, 4-5, 6-7 - share the same slice
@@ -34,7 +39,7 @@ int main()
     Servo{6}, Servo{7},
   };
 
-  std::array<ADC, 4> adcs{
+  std::array<ADC, 4> const adcs{
     ADC{ADC::Port::ADC0},   // joystick left:  OX
     ADC{ADC::Port::ADC1},   // joystick left:  OY
     ADC{ADC::Port::ADC2},   // joystick right: OX
@@ -42,19 +47,20 @@ int main()
   };
 
   // maps from servo number, to ADC it uses
-  std::array<unsigned, servos.size()> servo2adc{ 0, 1, 2, 3, 3, 3 };
+  std::array<unsigned, servos.size()> const servo2adc{ 0, 1, 2, 3, 3, 3 };
 
   while(true)
   {
-    std::array<float, adcs.size()> reads;
+    std::array<uint8_t, adcs.size()> pos;
     for(auto i=0u; i<adcs.size(); ++i)
-      reads[i] = adcs[i].read();
-  }
+      pos[i] = volt2pos( adcs[i].read() );
 
-  for(uint8_t p=0; true; ++p)
-  {
-    for(auto& s: servos)
-      s.pos(p);
+    for(auto i=0u; i<servos.size(); ++i)
+    {
+      auto const adc_i = servo2adc[i];
+      servos[i].pos( pos[adc_i] );
+    }
+
     sleep_ms(20);
   }
 }
